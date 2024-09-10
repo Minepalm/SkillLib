f8f7ca3b610c7ef6c3d637bb47696f747deeb3a3
karn <qkrehf2ggwp@gmail.com>
2024-09-10 21:43:53 +0900

    실린더히트박스 인식못하는 버그 수정

--- src/main/java/org/karn/skilllib/collider/Cylinder.java
+++ src/main/java/org/karn/skilllib/collider/Cylinder.java
@@ -7,7 +7,9 @@ import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 
 import static java.lang.Math.abs;
@@ -22,12 +24,12 @@ public class Cylinder extends Collider{
 
     public Cylinder(Location l, double up,double down,double radius){
         super(l.getWorld());
-        double height = center.getY() - down + ((up+down)*0.5d);
+        double height = l.getY() - down + ((up+down)*0.5d);
         this.center = l.toVector().setY(height);
         this.radius = radius;
         this.up = up;
         this.down = down;
-        Vector top = center.clone().setY(center.getY()+ ((up+down)*0.5d) );
+        Vector top = center.clone().setY(center.getY() + ((up+down)*0.5d));
         this.diagonal = this.center.distance(top);
     }
 
@@ -36,23 +38,27 @@ public class Cylinder extends Collider{
     }
 
     public boolean isCollide(BoundingBox box) {
-        if( abs(center.getY()-box.getMin().getY()) > abs(max(up,down)) + box.getHeight()){
-            return false;
-        }
-        Vector centerbox = box.getCenter();
-        double halfWidthX = box.getWidthX() / 2.0;
-        double halfHeight = box.getHeight() / 2.0;
-
-        double closestX = Math.max(centerbox.getX() - halfWidthX, Math.min(center.getX(), centerbox.getX() + halfWidthX));
-        double closestZ = Math.max(centerbox.getZ() - halfWidthX, Math.min(center.getZ(), centerbox.getZ() + halfWidthX));
-
-        double distanceSquared = new Vector(closestX, center.getY(), closestZ).distanceSquared(center);
-        if (distanceSquared > Math.pow(radius, 2)) {
+        if(center.getY() + ((up+down)*0.5d) < box.getMinY() || center.getY() - ((up+down)*0.5d) > box.getMaxY()){
             return false;
         }
 
-        double closestY = Math.max(centerbox.getY() - halfHeight, Math.min(center.getY(), centerbox.getY() + halfHeight));
-        return !(closestY < center.getY() - down || closestY > center.getY() + up);
+        for (Vector point : getPoints(box)) {
+            Vector centerTemp = center.clone().setY(0);
+            double dis = centerTemp.distance(point);
+            if (dis < radius) return true;
+        }
+        return false;
+    }
+
+    private static List<Vector> getPoints(BoundingBox box){
+        Vector min = box.getMin();
+        Vector max = box.getMax();
+        List<Vector> points = new ArrayList<>();
+        points.add(new Vector(min.getX(),0,min.getZ()));
+        points.add(new Vector(min.getX(),0,max.getZ()));
+        points.add(new Vector(max.getX(),0,min.getZ()));
+        points.add(new Vector(max.getX(),0,max.getZ()));
+        return points;
     }
     //-----------------------------------------------------------------------------------------------------------------------
     public Collection<Entity> getEntities(@Nullable Predicate<Entity> predicate){
